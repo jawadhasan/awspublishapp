d5d002556974d33089b070ea585e25cd9767c067
Jawad Shani <jawad.shani@hitachi-eu.com>
2021-05-16 02:23:03 +0200

    netcore published app

--- sqlscripts/insertProducts.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO products( name, serialno)
-VALUES
-( 'Product A', 'A12345678'),
-( 'Product B', 'B12345678'),
-( 'Product C', 'C12345678'),
-( 'Product D', 'D12345678'),
-( 'Product E', 'E12345678')
